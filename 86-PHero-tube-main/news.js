87d6310e29a15640876859f14c40e47b770cfbc6
shahidulgod <shahidulgod@gmail.com>
2023-09-02 19:56:34 +0600

    cading

--- 86-PHero-tube-main/news.js
+++ 86-PHero-tube-main/news.js
@@ -25,23 +25,20 @@ const viewCategory = async (category_id) => {
     console.log(categories)
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="card w-80  bg-base-100 shadow-xl">
+    <div class="card w-80 bg-base-100 shadow-xl">
         <figure class="px-10 pt-10">
-          <img 
-          src=${categories.thumbnail
-          }
-          />
+          <img src="${categories.thumbnail
+          }" />
+          <h4 class="text-right absolute bottom-44 right-6 text-lime-400" > ${categories.others.posted_date} </h4>
         </figure>
         <div class="card-body items-center text-center">
-          <div class= " flex justify-evenly> 
-          <img 
-          // src=${categories.authors
-          }
-          />
-          <h> </h>
-          </div
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          
+          <div class="flex gap-4 ">
+            <img class="h-12 w-12 rounded-full " src="${categories.authors[0].profile_picture}" alt="">
+            <h2 class="card-title">${categories.title
+            } </h2>
+          </div>
+          <p>${categories.authors[0].profile_name } </p>
+          <h4> view ${ categories.others.views  } </h4>
         </div>
       </div>
     `;
