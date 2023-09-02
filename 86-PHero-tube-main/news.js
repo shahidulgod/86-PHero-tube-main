e28d3c295584cd1ba3cd9f797e611a05c497d496
shahidulgod <shahidulgod@gmail.com>
2023-09-02 22:43:43 +0600

    massageadd

--- 86-PHero-tube-main/news.js
+++ 86-PHero-tube-main/news.js
@@ -18,36 +18,54 @@ const viewCategory = async (category_id) => {
   const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
   const data = await response.json(); 
   const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = '';
   
-  
-  // console.log(data.data);
-  data.data.forEach((categories) => {
-    console.log(categories)
-    const div = document.createElement('div');
-    div.innerHTML = `
-    <div class="card w-80 bg-base-100 shadow-xl">
-        <figure class="px-10 pt-10">
-          <img src="${categories.thumbnail
-          }" />
-          <h4 class="text-right absolute bottom-44 right-6 text-lime-400" > ${categories.others.posted_date} </h4>
-        </figure>
-        <div class="card-body items-center text-center">
-          <div class="flex gap-4 ">
-            <img class="h-12 w-12 rounded-full " src="${categories.authors[0].profile_picture}" alt="">
-            <h2 class="card-title">${categories.title
-            } </h2>
-          </div>
-          <p>${categories.authors[0].profile_name } </p>
-          <h4> view ${ categories.others.views  } </h4>
-        </div>
-      </div>
+  if (data.data.length === 0) {
+    // Display the error message when no categories are found
+    const errorMessage = document.createElement('div');
+    errorMessage.innerHTML = `
+    <div class=" mx-auto  lg:pl-[500px] mt-24"> 
+    <div class="card w-[600px] bg-base-100     shadow-xl">
+  <figure><img src="./Icon.png" alt="Shoes" /></figure>
+  <div class="card-body">
+    
+    <p class="text-center"> Oops!! Sorroy, there is no <br> content here </p>
+    
+  </div>
+</div>
+    </div>
     `;
-
-cardContainer.appendChild(div);
-  });
-
-
-
-
+    cardContainer.appendChild(errorMessage);
+  } else {
+    // Loop through and display the categories as usual
+    data.data.forEach((categories) => {
+      console.log(categories)
+      const div = document.createElement('div');
+      div.innerHTML = '';
+      div.innerHTML = `
+        <div class="card w-80 bg-base-100 shadow-xl">
+          <figure class="px-10 pt-10">
+            <img src="${categories.thumbnail}" />
+            <h4 class="text-right absolute bottom-44 right-6 text-lime-400">${categories.others.posted_date}</h4>
+          </figure>
+          <div class="card-body items-center text-center">
+            <div class="flex gap-4">
+              <img class="h-12 w-12 rounded-full" src="${categories.authors[0].profile_picture}" alt="">
+              <h2 class="card-title">${categories.title}</h2>
+            </div>
+            <p>${categories.authors[0].profile_name}</p>
+            <h4>view ${categories.others.views}</h4>
+          </div>
+        </div>
+      `;
+      cardContainer.appendChild(div);
+    });
+  }
 };
+
+
+// sorting here
+
+
+
 handleCategory()
